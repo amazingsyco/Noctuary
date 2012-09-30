74c8d3e4e1ebb893455bbecf972f63521d152a9d
Steve Streza <stevestreza@gmail.com>
2012-09-29 17:00:14 -0700

    It does something, woo hoo

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+var async = require("async");
+var config = require("./lib/config");
 var pipeline = require("./lib/pipeline");
-pipeline.build(function(err, results){
-	console.log("Built");
+
+var configSites = config.sites;
+
+var sites = [];
+for(var idx = 2; idx < process.argv.length; idx++){
+	var siteName = process.argv[idx];
+	if(configSites[siteName]){
+		sites.push(process.argv[idx]);
+	}
+}
+
+if(sites.length == 0){
+	for(var key in configSites){
+		sites.push(key);
+	}
+}
+
+async.forEachSeries(sites, function(sitename, cb){
+	var site = configSites[sitename];
+	pipeline.build(site, function(err, results){
+		console.log("Built");
+	});
 });
